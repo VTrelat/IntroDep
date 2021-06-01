b739d6c4c3e2ec5bdd7a9198d95a60955a5feaf7
Vincent Trélat <vincent.trelat@free.fr>
2021-06-01 11:14:16 +0200

    Modification du code

--- td1.c
+++ td1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+int squared(int x){
+    return x*x;
+};
+
+
 
 
 int main(void){
     int a=5;
     printf("Hello World!\na = %d\n", a);
+    printf("\na^2 = %d\n", squared(a));
     return 0;
 }
