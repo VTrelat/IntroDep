29a19d400e83ab9b5e7fd5aaa0925510cfa893b0
Vincent Trélat <vincent.trelat@free.fr>
2021-06-01 15:50:46 +0200

    Ajout de vect.c pour afficher un tableau

---
+++ vect.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void print_tableau(int t[], int size){
+    for(int i=0; i<size; i++){
+        printf("%d ", *(t+i));
+    }
+}
+
+int main(){
+    int n=100;
+    int size=n/2;
+    int t[size];
+    for(int i=0; i<size; i++){
+        t[i]=2*i;
+    }
+    print_tableau(t, size);
+    return 0;
+}
