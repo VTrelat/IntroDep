7a7aefe0e2bfe75f42c5747bf485c1eedab78efa
Vincent Trélat <vincent.trelat@free.fr>
2021-06-01 15:32:24 +0200

    module.c update

--- module.c
+++ module.c
@@ -5,8 +5,26 @@ int addone(int x){
     return x+1;
 }
 
+int fibo(int n){
+    int x1=1;
+    int x2=1;
+    if(n==0){
+        return x1;
+    }
+    if(n==1){
+        return x2;
+    }
+    else{
+        for(int i = 2; i<=n; i++){
+            x2=x2+x1;
+            x1=x2-x1;
+        }
+        return x2;
+    }
+}
+
 int main(void){
-    int x=5;
-    printf("%d + 1 = %d\n", x, addone(x));
+    int n=50;
+    printf("%dth Fibonacci term : %d\n", n, fibo(n));
     return 0;
 }
